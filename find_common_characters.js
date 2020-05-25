c8a43512da1a7806d435b84a1e8ee87c219197d0
VPoshelyuk <aslava198@gmail.com>
2020-05-24 20:27:15 -0400

    find common characters solved

---
+++ find_common_characters.js
@@ -0,0 +1,46 @@
+// Given an array A of strings made only from lowercase letters, 
+// return a list of all characters that show up in all strings within the list (including duplicates).  
+// For example, if a character occurs 3 times in all strings but not 4 times, 
+// you need to include that character three times in the final answer.
+
+// You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: ["bella","label","roller"]
+// Output: ["e","l","l"]
+// Example 2:
+
+// Input: ["cool","lock","cook"]
+// Output: ["c","o"]
+ 
+
+// Note:
+
+// 1 <= A.length <= 100
+// 1 <= A[i].length <= 100
+// A[i][j] is a lowercase letter
+
+// Credit: https://leetcode.com/problems/find-common-characters/
+
+/**
+ * @param {string[]} A
+ * @return {string[]}
+ */
+var commonChars = function(A) {
+    if(A.length==0)return []
+    return A[0].split('').map(m=>{
+        for(var i=1;i<A.length;i++){
+            var index = A[i].indexOf(m)
+            if(index==-1){
+                return null
+            }
+            else{
+                A[i] = A[i].replace(m,'')
+            }
+        }
+        return m
+    }).filter(m=>m!=null)
+};
